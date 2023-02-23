3073d4bf3fa4ceb3d7963afa0b44ca789391e8a2
Amin Limbada <aminlimbada94@gmail.com>
2023-02-23 12:54:02 +0400

    chore: update svg

--- packages/rainbowkit/src/wallets/walletConnectors/xdefiWallet/xdefiWallet.ts
+++ packages/rainbowkit/src/wallets/walletConnectors/xdefiWallet/xdefiWallet.ts
@@ -12,21 +12,17 @@ export const xdefiWallet = ({
   chains,
   shimDisconnect,
 }: XDEFIWalletOptions): Wallet => {
-  const installed =
-    typeof window !== 'undefined' &&
-    typeof window.ethereum !== 'undefined' &&
-    ((window.ethereum as any).isXDEFI === true ||
-      window.ethereum.providers?.find((p: any) => p.isXDEFI === true));
-
   return {
     id: 'xdefi',
     name: 'XDEFI Wallet',
-    installed: !!installed,
+    installed:
+      typeof window !== 'undefined' &&
+      (window.ethereum as any)?.isXDEFI === true,
     iconUrl: async () => (await import('./xdefiWallet.svg')).default,
     iconBackground: '#fff',
     downloadUrls: {
       browserExtension:
-        'https://install.xdefi.io/?utm_source=rainbowKit&utm_medium=organic&utm_campaign=xdefi.io&utm_id=xdefi.io',
+        'https://chrome.google.com/webstore/detail/xdefi-wallet/hmeobnfnfcmdkdcmlblgagmfpfboieaf',
     },
     createConnector: () => ({
       connector: new InjectedConnector({
